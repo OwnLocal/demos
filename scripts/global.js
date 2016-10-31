bd55fbbd650efcb766fe3d20e0a8ac6640e4d19b
Jorge Gonzalez <jorge@ownlocal.com>
2016-10-31 16:05:07 -0500

    Improvements to previewer

--- scripts/global.js
+++ scripts/global.js
@@ -2,3 +2,17 @@
 $(document).on('click', '.hero-to-content', function(){
   $("html, body").animate({ scrollTop: $('#products').offset().top }, 500);
 });
+
+// Load URL depending on parameter
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+  }
+};
