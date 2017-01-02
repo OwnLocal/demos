4bcede4315350952f41ffcd005474bd5d3f74265
Jorge Gonzalez <jorge@ownlocal.com>
2017-01-02 11:06:37 -0600

    Microsite

---
+++ microsite/main.js
@@ -0,0 +1,24 @@
+$(window).load(function() {
+  $('body').addClass('loaded');
+});
+// Go to content from hero
+$(document).on('click', '.nav-item a, .hero a', function(){
+  $("html, body").animate({ scrollTop: $('.section.' + $(this).attr('data-target')).offset().top }, 500);
+});
+
+/* Instagram */
+$(document).ready(function() {
+  instagram = "https://www.instagram.com/" + $('#about').attr('data-instagram-username') + "/media/";
+  $.getJSON( instagram, function( data ) {
+    items = [];
+    $.each( data, function( item, card ) {
+      cardOpening = "<div class='cn-card " + card.service + "'>";
+      cardClosing = "</div><div class='cn-date'>" + formattedCardDate + "</div></div>";
+      items.push( cardOpening + cardClosing);
+    });
+    $( "<div>", {
+      "class": "cn-feed",
+      html: items.join("")
+    }).appendTo("#about");
+  });
+});
