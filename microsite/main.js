47561bb85fd83e97d5c5da441bbec6f059817ffe
Jorge Gonzalez <jorge@ownlocal.com>
2017-01-19 12:50:59 -0600

    Microsite version 170119-1250

--- microsite/main.js
+++ microsite/main.js
@@ -1,51 +1,50 @@
 // Body classes
 $(window).load(function() {
   $('body').addClass('loaded');
-  if(window.location.href.indexOf("?generic=true") > -1) {
+  if (window.location.href.indexOf("?generic=true") > -1) {
     $("body").addClass("generic");
     $('.cover-wrapper').css('background-image', '');
   }
 });
 
 // Go to content from hero
-$(document).on('click', '.nav-item a, .cta-container a', function(){
-  $("html, body").animate({ scrollTop: $('.section.' + $(this).attr('data-target')).offset().top }, 500);
+$(document).on('click', '.nav-item a, .cta-container a', function() {
+  $("html, body").animate({
+    scrollTop: $('.section.' + $(this).attr('data-target')).offset().top
+  }, 500);
 });
 
 // Go to top
-$(document).on('click', '.go-to-top', function(){
-  $("html, body").animate({ scrollTop: 0 }, 500);
+$(document).on('click', '.go-to-top', function() {
+  $("html, body").animate({
+    scrollTop: 0
+  }, 500);
   return false;
 });
 
-
 $(document).ready(function() {
-  
   // Show go to top
- 	$(window).on("scroll", function () {
+  $(window).on("scroll", function() {
     if ($(this).scrollTop() > 100) {
       $(".go-to-top").show();
-    }
-    else {
+    } else {
       $(".go-to-top").hide();
     }
   });
-
   // Photo gallery
-  $(".photo-gallery .thumbs img").on("click", function(){
+  $(".photo-gallery .thumbs img").on("click", function() {
     $('.active-image').show();
     $('.active-image img').attr('src', $(this).attr('data-src').replace('.thumbs', 'large'));
     $('body').addClass('overflow--hidden');
   });
-  $(".photo-gallery .photo-gallery-close").on("click", function(){
+  $(".photo-gallery .photo-gallery-close").on("click", function() {
     $('.active-image').hide();
     $('body').removeClass('overflow--hidden');
   });
-  $(".active-image").on("click", function(){
+  $(".active-image").on("click", function() {
     $(this).hide();
     $('body').removeClass('overflow--hidden');
   });
-
 });
 
 // Close with ESC
@@ -55,4 +54,3 @@ $(document).keyup(function(e) {
     $('.photo-gallery .active-image').hide();
   }
 });
-
