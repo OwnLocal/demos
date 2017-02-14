43ce63b6d9e3538f9f14163d7ee7754bb297cdbb
Jorge Gonzalez <jorge@ownlocal.com>
2017-02-14 15:42:08 -0600

    Media Kit demo site

---
+++ media-kit/main.js
@@ -0,0 +1,11 @@
+// Body classes
+$(window).load(function() {
+  $('body').addClass('loaded');
+});
+
+// Go to content from hero
+$(document).on('click', '.nav-item a, .cta-container a', function() {
+  $("html, body").animate({
+    scrollTop: $('.section.' + $(this).attr('data-target')).offset().top
+  }, 500);
+});
