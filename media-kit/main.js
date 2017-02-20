77bc8384b04454b4a4375d997ee4750dd11f3c25
Jorge Gonzalez <jorge@ownlocal.com>
2017-02-20 16:32:51 -0600

    Updates

--- media-kit/main.js
+++ media-kit/main.js
@@ -1,11 +1,16 @@
-// Body classes
-$(window).load(function() {
-  $('body').addClass('loaded');
-});
-
 // Go to content from hero
 $(document).on('click', '.hero button', function() {
   $("html, body").animate({
     scrollTop: $('.section.' + $(this).attr('data-target')).offset().top
   }, 500);
+});
+
+// Add class to body when cover view is no longer in viewport
+$(window).scroll(function() {    
+  var scroll = $(window).scrollTop();    
+  if (scroll >= $(window).height()) {
+    $("body").addClass("scrolled");
+  } else {
+    $("body").removeClass("scrolled");
+  }
 });
